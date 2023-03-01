c55e95a32e470f0c0311eada7087123be7c7a67d
Андрій Шмигельський <angey0073@gmail.com>
2023-03-01 18:11:46 +0200

    lab1

---
+++ src/lab1.java
@@ -0,0 +1,22 @@
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        int a=8;
+        int b=3;
+        System.out.println("Рузультат: "+ a/b);
+        System.out.println("Рузультат: "+ a%b);
+        /////
+        System.out.println("Task 2");
+        int c=45 ;
+        int te=c/10;
+        int on=c%10;
+        System.out.println("Рузультат: "+ (te+on));
+        /////
+        System.out.println("Task 3");
+        double d=2444.3444134;
+        System.out.println("Результат: "+Math.round(d));
+
+
+    }
+
+}
